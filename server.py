e9e1fc17f1e30d1f318edc6066e9d52cb5f7a882
Aleksey Loktionov <sams12.34.58@gmail.com>
2025-02-17 18:34:33 +0500

    add bd responce for registrtion

--- server.py
+++ server.py
@@ -12,6 +12,9 @@ my_db = mysql.connector.connect(
 )
 my_cursor = my_db.cursor()
 insert_reg = "INSERT INTO users (id, login, password) VALUES (%s, %s, %s)"
+select_id =  "SELECT id FROM users ORDER BY id DESC LIMIT 1"
+select_login = "SELECT login FROM users WHERE login = %s;"
+
 
 app = Flask("server")
 history = []
@@ -21,6 +24,7 @@ test_reg = []
 def login():
     if request.method == 'POST':
         history.append(request.get_json())
+        print(request.get_json())
         return request.get_json()
     else:
         try:
@@ -31,7 +35,12 @@ def login():
 @app.route('/registration', methods = ['GET', 'POST'])
 def registration():
     if request.method == 'POST':
-        test_reg.append(request.get_json())
+        responce = request.get_json()
+        my_cursor.execute(select_id)
+        id_result = my_cursor.fetchone()
+        person = (id_result[0] + 1, responce['login'],  responce['password'])
+        my_cursor.execute(insert_reg, person)
+        my_db.commit()
         return request.get_json()
     else:
         try:
