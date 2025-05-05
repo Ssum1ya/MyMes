86a27201546c27841b4fb37e20c0a67fcd85737f
Aleksey Loktionov <sams12.34.58@gmail.com>
2025-05-05 18:22:34 +0500

    add test.py workly frontend add client.py not workly frontend add login.png Without backend

--- app/client.py
+++ app/client.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Button, Entry, Listbox, Frame, Scrollbar, Text
+from tkinter import Tk, Label, Button, Entry, Listbox, Frame, Scrollbar, Text, PhotoImage
 from tkinter import RIGHT, LEFT, BOTH, Y, END
 from tkinter import messagebox
 import requests
@@ -8,8 +8,8 @@ from time import sleep
 from ServerResponceHandler import ServerResponceHandler
 
 root = Tk()
-root.geometry('400x600')
-root.title('Войти в систему')
+# root.geometry('400x600')
+# root.title('Войти в систему')
 
 login_password_id__array = []
 thread_flag = True
@@ -164,24 +164,79 @@ def choise():
 
 def registration():
     clear()
-    main_title = Label(text = 'Для входа в систему зарегистрируйтесь')
-    button_back = Button(text = 'Назад', command = choise)
-    log_title = Label(text = 'Введите Ваш логин')
-    reg_login = Entry()
-    title_password1 = Label(text = 'Введите Ваш пароль')
-    reg_password1 = Entry()
-    title_password2 = Label(text = 'Еще раз пароль:')
-    reg_password2 = Entry(show = '*')
-    button_regist = Button(text = 'Зарегистрироваться', command = lambda: reg(reg_login.get(), reg_password1.get(), reg_password2.get()))
-    main_title.pack()
-    button_back.pack()
-    log_title.pack()
-    reg_login.pack()
-    title_password1.pack()
-    reg_password1.pack()
-    title_password2.pack()
-    reg_password2.pack()
-    button_regist.pack()
+    root.title('Login')
+    root.geometry('925x500+300+200')
+    root.configure(bg = "#fff")
+    root.resizable(False, False)
+
+    img = PhotoImage(file = 'C:/Users/Proger/Documents/MyMes/login.png')
+    Label(root, image = img, bg = 'white').place(x = 50, y = 50)
+
+    frame = Frame(root, width = 350, height = 350, bg = "white")
+    frame.place(x = 480, y = 70)
+
+    heading = Label(frame, text = 'Sign in', fg = '#57a1f8', bg = 'white', font = ('Microsoft YaHei UI Light', 23, 'bold'))
+    heading.place(x = 100, y = 5)
+
+    def on_enter(e):
+        user.delete(0, 'end')
+
+    def on_leave(e):
+        name = user.get()
+        if name == '':
+            user.insert(0, 'Username')
+
+    user = Entry(frame, width = 25, fg = 'black', border = 0, bg = "white", font = ('Microsoft YaHei UI Light', 11))
+    user.place(x = 30, y = 80)
+    user.insert(0, 'Username')
+    user.bind('<FocusIn>', on_enter)
+    user.bind('<FocusOut>', on_leave)
+
+    Frame(frame, width = 295, height = 2, bg = 'black').place(x = 25, y = 107)
+
+
+    def on_enter(e):
+        code.delete(0, 'end')
+
+    def on_leave(e):
+        name = code.get()
+        if name == '':
+            code.insert(0, 'Username')
+
+    code = Entry(frame, width = 25, fg = 'black', border = 0, bg = 'white', font = ('Microsoft YaHei UI Light', 11))
+    code.place(x = 30, y = 150) 
+    code.insert(0, 'Password')
+    code.bind('<FocusIn>', on_enter)
+    code.bind('<FocusOut>', on_leave)
+
+    Frame(frame, width = 295, height = 2, bg = 'black').place(x = 25, y = 177)
+
+    Button(frame, width = 39, pady = 7, text = 'Sign in', bg = '#57a1f8', fg = 'white', border = 0, command = lambda: log(user.get(), code.get())).place(x = 35, y = 204)
+    label = Label(frame, text = "Don't have an account?", fg = 'black', bg = 'white', font = ('Microsoft YaHei UI Light', 9))
+    label.place(x = 75, y = 270)
+
+    sign_up = Button(frame, width = 6, text = 'Sign up', border = 0, bg = 'white', cursor = 'hand2', fg = '#57a1f8')
+    sign_up.place(x = 215, y = 270)
+
+
+    # main_title = Label(text = 'Для входа в систему зарегистрируйтесь')
+    # button_back = Button(text = 'Назад', command = choise)
+    # log_title = Label(text = 'Введите Ваш логин')
+    # reg_login = Entry()
+    # title_password1 = Label(text = 'Введите Ваш пароль')
+    # reg_password1 = Entry()
+    # title_password2 = Label(text = 'Еще раз пароль:')
+    # reg_password2 = Entry(show = '*')
+    # button_regist = Button(text = 'Зарегистрироваться', command = lambda: reg(reg_login.get(), reg_password1.get(), reg_password2.get()))
+    # main_title.pack()
+    # button_back.pack()
+    # log_title.pack()
+    # reg_login.pack()
+    # title_password1.pack()
+    # reg_password1.pack()
+    # title_password2.pack()
+    # reg_password2.pack()
+    # button_regist.pack()
 
 def reg(user_login, password1, password2):
     if password1 != password2:
@@ -230,5 +285,6 @@ def log(user_login, user_password):
         messagebox.showinfo('Ошибка', 'войти так как не удалось найти совпадения')
         login()
 
-choise()
+#choise()
+registration()
 root.mainloop()
