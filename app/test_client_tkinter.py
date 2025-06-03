3ec84b4ca1615c6a68532f6055a5b5d78695a2e8
Aleksey Loktionov <sams12.34.58@gmail.com>
2025-06-04 00:29:57 +0500

    add files into db

--- app/test_client_tkinter.py
+++ app/test_client_tkinter.py
@@ -1,20 +1,36 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from tkinter import filedialog
+import requests
+import base64
+import json
 
 root = Tk()
 root.title("METANIT.COM")
 root.geometry("250x200")
 
-def open_file():
+login1 = 'zxc'
+login2 = 'Aokihary'
+
+def send_file():
     filepath = filedialog.askopenfilename()
     if filepath != "":
+        file_name = filepath.split('/')[-1]
         with open(filepath, "rb") as file:
-            text = file.read()
-            print(text)
+            data = file.read()
+            data_b64 = base64.b64encode(data).decode('utf-8')
+            request = requests.post('http://127.0.0.1:5000/send_files', json = {'login1' : login1,
+                                                                     'login2' : login2,
+                                                                     'file_name' : file_name,
+                                                                     'file_data' : data_b64})
+            server_answer = json.loads(request.content.decode())
+            answer = server_answer['answer']
+            if answer == 'Denied':
+                messagebox.showinfo('Ошибка', 'Файл с таким названием уже есть')
+            else:
+                messagebox.showinfo('Успешно', 'Файл отправлен')
 
-
-open_button = ttk.Button(text="Открыть файл", command=open_file)
+open_button = ttk.Button(text="Открыть файл", command=send_file)
 open_button.grid(column=0, row=1, sticky=NSEW, padx=10)
 
 root.mainloop()
--- app/test_server.py
+++ app/test_server.py
@@ -1,14 +1,67 @@
 from flask import Flask, request, send_file
+import base64
+import os
+
+import mysql.connector
+import databaseTools.database_config as cfg
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host = cfg.HOST,
+        user = cfg.USER,
+        auth_plugin = cfg.AUTH_PLAGIN,
+        passwd = cfg.PASSWORD,
+        database = cfg.DATABASE
+    )
 
 app = Flask("server")
 file_name = 'kiyotaka.jpg'
+server_files_path = 'C:/Users/Proger/Desktop/server_files/'
 
-@app.route('/login', methods = ['GET', 'POST'])
+insert_file = 'INSERT INTO files (id, login1, login2, name, path) VALUES (%s, %s, %s, %s, %s)'
+select_files_lastId =  'SELECT id FROM files ORDER BY id DESC LIMIT 1'
+
+@app.route('/send_files', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return send_file(file_name, as_attachment = True, download_name = 'zxc.jpg')
     elif request.method == 'POST':
-        pass
+        responce = request.get_json()
+        db = get_db_connection()
+        cursor = db.cursor()
+
+        name = responce['file_name']
+        file_data = base64.b64decode(responce['file_data'])
+        login1, login2 = responce['login1'], responce['login2']
+
+        path = None
+        path1 = server_files_path + login1 + '-' + login2
+        path2 = server_files_path + login2 + '-' + login1
+        if not os.path.exists(path1) and not os.path.exists(path2):
+            os.mkdir(path1)
+            path = path1
+        elif os.path.exists(path1): path = path1
+        elif os.path.exists(path2): path = path2
+        
+        if os.path.exists(path + '/' + name):
+            return {'answer' : 'Denied'}
+
+        with open(path + '/' + name, 'wb') as file:
+            file.write(file_data)
+
+        cursor.execute(select_files_lastId)
+        id = None
+        last_id = cursor.fetchone()
+        if last_id == None: id = 1
+        else: id = last_id[0] + 1
+
+        add_file = (id, login1,  login2, name, path)
+        cursor.execute(insert_file, add_file)
+        
+        db.commit()
+        cursor.close()
+        db.close()   
+        return {'answer' : 'Succes'}
 
 if __name__ == '__main__':
     app.run(debug = True)
